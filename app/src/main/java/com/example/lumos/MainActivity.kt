d39e560bcd040ee31d20d95059cfd6a21ff71ef7
sinha-shaurya <shaurya.sinha@protonmail.com>
2021-04-10 19:13:25 +0530

    Added persistence to navigation component

--- app/src/main/java/com/example/lumos/MainActivity.kt
+++ app/src/main/java/com/example/lumos/MainActivity.kt
@@ -6,21 +6,28 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.lumos.databinding.ActivityMainBinding
+import com.example.lumos.utils.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController:LiveData<NavController>?=null
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
+        binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val navController=findNavController(R.id.fragment)
+        //val navController=findNavController(R.id.fragment)
 
-        binding.bottomNav.setupWithNavController(navController)
+        //binding.bottomNav.setupWithNavController(navController)
 
         /*
         //setup action bar with navController with appbar configuration
@@ -31,8 +38,14 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController,appBarConfiguration)
 
          */
+
+        supportActionBar?.hide()
+        if(savedInstanceState==null){
+            setupBottomNavigationBar()
+        }
     }
 
+    /*
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home->{
@@ -42,5 +55,33 @@ class MainActivity : AppCompatActivity() {
             else->super.onOptionsItemSelected(item)
         }
        return true
+    }*/
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
     }
+    private fun setupBottomNavigationBar(){
+        val navGraphIds=listOf(R.navigation.blog,R.navigation.events,R.navigation.practice,R.navigation.login)
+
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val controller=bottomNavigationView.setupWithNavController(
+            navGraphIds=navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fragment,
+            intent=intent
+        )
+
+        //use this to setup Action bar
+
+        controller.observe(this){navController->
+            setupActionBarWithNavController(navController)
+        }
+        currentNavController=controller
+    }
+
+    /*
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp()?:false
+    }*/
 }
